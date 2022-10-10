a82166eb003cc8aed5836ed1726dcbdf91f6b790
tmbuza <ndelly@gmail.com>
2022-10-10 15:18:05 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -153,14 +153,14 @@ if uploaded_file is not None:
   st.subheader("""Descriptive statistics""")
   st.dataframe(df.describe())
   
-  st.write("##")
-  st.subheader("""Scatter diagram""")  
-  fig, ax = plt.subplots(1,1)
-  sns.scatterplot(x=df.iloc[:, 0], y=df.iloc[:, 6], hue = df.iloc[:, 3]) # Change positional index to match the location of variables.
-  # ax.set_xlabel('X-axis values')
-  # ax.set_ylabel('Y-axis values')
-  st.pyplot(fig)
-  
+  # st.write("##")
+  # st.subheader("""Scatter diagram""")  
+  # fig, ax = plt.subplots(1,1)
+  # sns.scatterplot(x=df.iloc[:, 0], y=df.iloc[:, 6], hue = df.iloc[:, 3]) # Change positional index to match the location of variables.
+  # # ax.set_xlabel('X-axis values')
+  # # ax.set_ylabel('Y-axis values')
+  # st.pyplot(fig)
+  # 
   st.write("##")
   st.write("##")
   st.subheader("""Correlation heatmap""")
@@ -170,71 +170,3 @@ if uploaded_file is not None:
 else:
   st.warning(':point_left:  Awaiting user\'s input file :exclamation:')
 
-# st.write("##")
-# st.write("##")
-# st.write("---")
-#     
-# @st.cache(persist=True)
-# def split(df):
-#     y = df.target
-#     x = df.drop(columns=["target"])
-#     x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3, random_state=0)
-#     return x_train, x_test, y_train, y_test
-#   
-#     x_train, x_test, y_train, y_test = split(df)
-#     
-# def plot_metrics(metrics_list):
-#   if "Confusion Matrix" in metrics_list:
-#       st.subheader("Confusion Matrix")
-#       plot_confusion_matrix(model, x_test, y_test, display_labels=   class_names)
-#       st.pyplot()
-#   if "ROC Curve" in metrics_list:
-#       st.subheader("ROC Curve")
-#       plot_roc_curve(model, x_test, y_test)
-#       st.pyplot()
-#   if "Precision-Recall Curve" in metrics_list:
-#       st.subheader("Precision-Recall Curve")
-#       plot_precision_recall_curve(model, x_test, y_test)
-#       st.pyplot()
-# class_names = ["Benign", "Malignant"]
-#  
-# st.sidebar.subheader("Choose classifier")
-# classifier = st.sidebar.selectbox("Classifier", ("Support Vector Machine (SVM)", "Logistic Regression", "Random Forest"))
-# 
-# if classifier == "Support Vector Machine (SVM)":
-#     st.sidebar.subheader("Hyperparameters")
-#     C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key="C")
-#     kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key="kernel") 
-#     gamma = st.sidebar.radio("Gamma (Kernal coefficient", ("scale", "auto"), key="gamma")
-#     metrics = st.sidebar.multiselect("What metrics to plot?", ("Confusion Matrix", "ROC Curve", "Precision-Recall Curve"))
-#     
-#     if st.sidebar.button("Classify", key="classify"):
-#       st.subheader("Support Vector Machine (SVM) results")
-#       df = load()
-#       x_train, x_test, y_train, y_test = split(df)
-#       model = SVC(C=C, kernel=kernel, gamma=gamma)
-#       model.fit(x_train, y_train)
-#       accuracy = model.score(x_test, y_test)
-#       y_pred = model.predict(x_test)
-#       st.write("Accuracy: ", accuracy.round(2))
-#       st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
-#       st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2)) 
-#       plot_metrics(metrics)
-# 
-# if classifier == "Logistic Regression":
-#     st.sidebar.subheader("Hyperparameters")
-#     C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key="C_LR")
-#     max_iter = st.sidebar.slider("Maximum iterations", 100, 500, key="max_iter")
-#     metrics = st.sidebar.multiselect("What metrics to plot?", ("Confusion Matrix", "ROC Curve", "Precision-Recall Curve"))
-#     
-#     if st.sidebar.button("Classify", key="classify"):
-#         st.subheader("Logistic Regression Results")
-#         model = LogisticRegression(C=C, max_iter=max_iter)
-#         model.fit(x_train, y_train)
-#         accuracy = model.score(x_test, y_test)
-#         y_pred = model.predict(x_test)
-#         
-#         st.write("Accuracy: ", accuracy.round(2))
-#         st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
-#         st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
-#         plot_metrics(metrics)
